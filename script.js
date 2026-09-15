083b7b983cce722b966c71b08cb1b86f8008cfa6
Tiago Reis <ttrpk@hotmail.com>
2026-09-15 19:36:14 -0300

    feat: Função Check

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Número Correto!!🏆🏆';
+
+// document.querySelector('.number').textContent = 13;
+//
+
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Número não informado ⛔';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Número Correto!!🏆🏆';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        'Número maior que o correto ⚠';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '❌GAME OVER!!❌';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        'Número menor que o correto ⚠';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '❌GAME OVER!!❌';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+console.log(secretNumber);
