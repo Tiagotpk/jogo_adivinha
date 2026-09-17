d99d6de691584e2f38d05e0dac3dcad6c92dbe3d
Tiago Reis <ttrpk@hotmail.com>
2026-09-17 19:03:33 -0300

    feat: Função Tentar Novamente

--- script.js
+++ script.js
@@ -11,8 +11,6 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -21,6 +19,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Número não informado ⛔';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Número Correto!!🏆🏆';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -46,6 +45,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
 });
 
 console.log(secretNumber);
