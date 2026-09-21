05fabc4d8dd2b935db30a8b7d975ea14531eafd9
Tiago Reis <ttrpk@hotmail.com>
2026-09-21 18:52:47 -0300

    REFATORANDO O CÓDIGO

--- script.js
+++ script.js
@@ -4,43 +4,55 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+function displayBody(body) {
+  document.querySelector('body').style.backgroundColor = body;
+}
+
+function displayNumber(number) {
+  document.querySelector('.number').textContent = number;
+}
+
+function displayScore(score) {
+  document.querySelector('.score').textContent = score;
+}
+
+function displayHighScore(highscore) {
+  document.querySelector('.highscore').textContent = highscore;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'Número não informado ⛔';
+    displayMessage('Número não informado ⛔');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Número Correto!!🏆🏆';
-    document.querySelector('.number').textContent = secretNumber;
-
-    document.querySelector('body').style.backgroundColor = '#60b347';
-
+    displayMessage('Número Correto!!🏆🏆');
+    displayNumber(secretNumber);
+    displayBody('#60b347');
     document.querySelector('.number').style.width = '30rem';
 
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      displayHighScore(highscore);
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        'Número maior que o correto ⚠';
+      displayMessage(
+        guess > secretNumber
+          ? 'Número maior que o correto ⚠'
+          : 'Número menor que o correto ⚠',
+      );
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = '❌GAME OVER!!❌';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent =
-        'Número menor que o correto ⚠';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '❌GAME OVER!!❌';
-      document.querySelector('.score').textContent = 0;
+      displayMessage('❌GAME OVER!!❌');
+      displayScore(0);
+      displayBody('#f44336');
     }
   }
 });
@@ -49,12 +61,12 @@ document.querySelector('.again').addEventListener('click', function () {
   // location.reload();
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector('.message').textContent = 'Comece a adivinhar...';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
+  displayMessage('Comece a adivinhar...');
+  displayScore(score);
+  displayNumber('?');
   document.querySelector('.guess').value = '';
 
-  document.querySelector('body').style.backgroundColor = '#222';
+  displayBody('#222');
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').focus();
 });
